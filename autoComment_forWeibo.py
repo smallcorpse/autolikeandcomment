4c1dee26f39c0952f8f435f972588f91adfd83d1
smallcorpse <smallcorpse.rui@qq.com>
2015-10-19 14:21:22 +0800

    change log

--- autoComment_forWeibo.py
+++ autoComment_forWeibo.py
@@ -27,6 +27,13 @@ keywordseq = [
 Commentusername = "smallcorpse@hotmail.com"
 Commentpassword = "l4g3s2b3lb"
 
+Commentseq = [
+    u"随机从一个评论中选取一个进行评论",
+    u"随机从一个评论中选取一个进行评论",
+    u"随机从一个评论中选取一个进行评论",
+    u"随机从一个评论中选取一个进行评论"
+]
+
 getquaninty = 0
 
 api = weibo.APIClient(app_key=APP_KEY,app_secret=APP_SECRET,redirect_uri=REDIRECT_URL)
@@ -58,6 +65,8 @@ def login(username, password):
     except:
         time.sleep(1)
 
+    log("appuser logined")
+
 login(username,password)
 
 code = browser.current_url.strip("https://api.weibo.com/oauth2/default.html?code=")
@@ -66,16 +75,21 @@ browser.quit()
 
 try:
     request = api.request_access_token(code,REDIRECT_URL)
+    log("request acces token")
     access_token = request.access_token
     expires_in = request.expires_in
     pickle.dump( access_token, open("access_token.pkl","wb"))
     pickle.dump( expires_in, open("expires_in.pkl","wb"))
+    log("token has been saved")
 
 except:
     access_token = pickle.load(open("access_token.pkl", "rb"))
     expires_in = pickle.load(open("expires_in.pkl", "rb"))
+    log("token has been loaded")
 
 api.set_access_token(access_token, expires_in)
+
+log("accessed weibo api")
 
 #https://api.weibo.com/2/comments/create.json
 
@@ -91,7 +105,7 @@ def search(searchWord):
     inputBtn.send_keys(searchWord.strip().decode("utf-8"))
     browser.find_element_by_class_name('searchBtn').click()
 
-    print "searching :" + searchWord
+    log("searching :" + searchWord)
 
 def userlogin(username, password):
 
@@ -115,12 +129,12 @@ def getid():
     msglist = browser.find_elements_by_xpath("//div[@action-type='feed_list_item']")
     for msg in msglist :
         if msg.get_attribute("mid"):
-            api.comments.create.post(comment=u'啦啦啦我在评论',id=int(msg.get_attribute("mid")))
-            print msg.get_attribute("mid")
+            api.comments.create.post(comment=choice(Commentseq),id=int(msg.get_attribute("mid")))
+            log (msg.get_attribute("mid") + " has been commend")
             try:
                 browser.find_element_by_class_name("W_close").click()
                 time.sleep(7200)
-                print "sleeping"
+                log("sleeping")
             except:
                 getquaninty += 1
                 time.sleep(choice([10,12,13,14]))
@@ -136,7 +150,7 @@ def nextpage():
         nextpagebtn.click()
     else:
         #browser.quit()
-        print "end"
+        log("end")
 
     time.sleep(5)
 
@@ -149,7 +163,7 @@ for i in range(0,50):
     getid()
     nextpage()
 
-print str(getquaninty)  + " messages got"
+log(str(getquaninty)  + " messages got")
 
 time.sleep(5)
 browser.quit()
